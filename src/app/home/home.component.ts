cd474bf619840c5b63c83b9f8459710cefd753ab
DarioAlor <A01731643@itesm.mx>
2021-11-03 16:45:44 -0600

    Token fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -42,7 +42,7 @@ export class HomeComponent implements OnInit {
   addItem() {
     /*this.goals.push(this.goalText);*/
 
-    var mytoken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFkc29mdCIsImV4cCI6MTYzNDc5NTgwMiwib3JpZ0lhdCI6MTYzNDc5NTUwMn0._6tOsFeS7Li59toIpxyD1rDNi_HXnjvcDPBjoKXNcUk";
+    var mytoken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImRhcmlvIiwiZXhwIjoxNjM1OTc5MzMyLCJvcmlnSWF0IjoxNjM1OTc5MDMyfQ.Aj_g6sW6qF79qW3YhArGwrdcEaqhsYUmhHjerrciLx0";
     //this.storageService.getSession("token");
     alert(this.goalText);
 
